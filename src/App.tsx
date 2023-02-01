6f0a41d0645aa5dccf339cf7b0766e8f948847c8
HersanKuang <mlnrmsw@gmail.com>
2023-02-01 10:15:40 +0800

    build: setup routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import routes from './router'
+import { useRoutes } from 'react-router-dom'
 
 function App() {
-  return <div className="App"></div>
+  return <div className="app">{useRoutes(routes)}</div>
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
@@ -12,9 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ThemeProvider theme={theme}>
-        <App />
-      </ThemeProvider>
+      <Suspense fallback="">
+        <ThemeProvider theme={theme}>
+          <App />
+        </ThemeProvider>
+      </Suspense>
     </BrowserRouter>
   </React.StrictMode>
 )
