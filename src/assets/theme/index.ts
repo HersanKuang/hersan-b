798a2133a8573184157bbbefabaef7c18bde85c8
HersanKuang <mlnrmsw@gmail.com>
2023-02-02 22:22:40 +0800

    feat: complete the theme color configuration

--- src/assets/theme/index.ts
+++ src/assets/theme/index.ts
@@ -4,4 +4,23 @@ const theme = {
   mixin: {}
 }
 
+export const light = {
+  color: {
+    body: '#fff'
+  },
+  text: {
+    main: '#161209'
+  }
+}
+
+export const dark = {
+  color: {
+    header: '#252627',
+    body: '#292a2d'
+  },
+  text: {
+    main: '#a9a9b3'
+  }
+}
+
 export default theme
